f094996735a9fe59b50110275d18ac7ca2bc7280
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-09-16 22:06:51 -0300

    button toggle theme style included, lottie animation logic changed to icons logic

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,24 +1,90 @@
 import Title from "./Title";
-
-
+import styled from "styled-components";
+import { useContext } from "react";
+import { AppContext } from "../../data/context/AppContext";
+import { IconMoon, IconSun } from "../icons";
 interface HeaderProps {
-
-
-    title: string;
-    subtitle: string;
-   
-
-
+  title: string;
+  subtitle: string;
 }
 
+const BoxHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ButtonTheme = styled.button`
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 1rem;
+  height: 2rem;
+  cursor: pointer;
+  width: 10rem;
+  background: ${(props) => props.bg};
+  color: ${(props) => props.color};
+  border: none;
+  display: flex;
+  flex-direction: ${(props) => props.direction};
+`;
+
+const BoxIcon = styled.div`
+  padding: 1rem;
+  width: 3.5rem;
+  align-items: center;
+  justify-content: space-around;
+  height: 1rem;
+  border-radius: 1rem;
+  display: flex;
+  border-radius: 5rem;
+  border: 1px solid white;
+`;
+
+const TitleButton = styled.p`
+  display: none;
+  font-size: 0.8rem;
+  font-weight: 600;
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+  }
+`;
 
 export default function Header(props: HeaderProps) {
+  const { theme, setTheme } = useContext(AppContext);
 
+  function changeTheme() {
+    console.log("teste");
 
-    return(
+    setTheme(theme === "light" ? "dark" : "light");
 
-        <div>
-            <Title title={props.title} subtitle={props.subtitle} />
-        </div>
-    )
-} 
+    window.localStorage.setItem(
+      "USER_THEME",
+      theme === "light" ? "dark" : "light"
+    );
+  }
+
+  const textTheme = {
+    light: "Light mode",
+    dark: "Dark mode",
+  };
+
+  return (
+    <BoxHeader>
+      <Title title={props.title} subtitle={props.subtitle} />
+      <ButtonTheme
+        onClick={changeTheme}
+        bg={
+          theme === "light"
+            ? "linear-gradient(to right, #a88b8b 40%, #3232b8)"
+            : "linear-gradient(to left, #d6c211 , #fc9105)"
+        }
+        direction={theme === "light" ? "row-reverse" : "row"}
+        color={theme === "light" ? "black" : "white"}
+      >
+        <TitleButton>
+          {theme === "light" ? textTheme.dark : textTheme.light}
+        </TitleButton>
+        <BoxIcon> {theme === "light" ? IconMoon : IconSun}</BoxIcon>
+      </ButtonTheme>
+    </BoxHeader>
+  );
+}
--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -4,8 +4,7 @@ import MenuLateral from "./SideMenu";
 import styled from "styled-components";
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../data/context/AppContext";
-import Lottie from 'react-lottie-player'
-import ThemeAnimation from "../../assets/lotties/thememode.json";
+
 
 interface LayoutProps {
   title: string;
@@ -19,59 +18,13 @@ const BoxLayout = styled.div`
   width: 100vw;
 `;
 
-const ButtonTheme = styled.button`
-  cursor: pointer;
-`;
 
-const DivThemeButton = styled.div `
-    width: 0rem;
-    cursor: pointer;
-    background-color: transparent;
-
-`
 
 export default function Layout(props: LayoutProps) {
-    
   const { theme, setTheme } = useContext(AppContext);
 
-  const [themeIcon, setThemeIcon] = useState({
-    isStopped: false, direction: null
-})
 
-
-
-
-
-  function changeTheme() {
-
-
-
-   
-    setThemeIcon({...themeIcon, isStopped:!themeIcon.isStopped, direction: theme === "light"? -1: 1
-        
-        
-        })
-
-        setTimeout(() => {
-            
-           
-   
-    setThemeIcon({...themeIcon, isStopped:false
-        
-        
-    })
-        }, 1400);
-  
-    
-
-
-    setTheme(theme === "light" ? "dark" : "light");
-
-    window.localStorage.setItem(
-      "USER_THEME",
-      theme === "light" ? "dark" : "light"
-    );
-  }
+ 
 
   return (
     <BoxLayout>
@@ -86,15 +39,7 @@ export default function Layout(props: LayoutProps) {
           padding: "4rem",
         }}
       >
-        <DivThemeButton onClick={changeTheme}>
-                <Lottie
-
-            animationData={ThemeAnimation}
-            direction={themeIcon.direction}
-             play={themeIcon.isStopped}
-            style={{ width: 80, height: 80 }}
-            />
-        </DivThemeButton>
+      
         <Header title={props.title} subtitle={props.subtitle} />
         <Content>{props.children}</Content>
       </div>
