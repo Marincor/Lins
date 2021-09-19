1d470e37926d442f6b96393b7f50bb8d8040262d
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-09-19 20:46:04 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@ const withImages = require('next-images')
 module.exports = withImages({
   webpack(config, options) {
     return config
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+    
+  },
 })
